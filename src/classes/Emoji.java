a8f79d7d6b51523f36c94fdf5b658f966f8e9b7e
Russel <russelcabigquez8@gmail.com>
2024-09-16 13:25:59 +0800

    third commit

--- src/classes/Emoji.java
+++ src/classes/Emoji.java
@@ -6,7 +6,6 @@ import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
